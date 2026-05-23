748b4fe8871bfe72b4d2369ee4d913e3d79f4575
GaboRReyes <reyesjosafat816@gmail.com>
2026-05-23 13:42:47 -0600

    feat: implement login functionality with email validation and token generation

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -6,9 +6,34 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+     public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+
+            return response()->json([
+                'message' => 'Credenciales inválidas'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('frontend')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
     public function token(Request $request)
     {
         $request->validate([
